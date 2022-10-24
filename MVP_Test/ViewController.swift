e00ce720c6be5640356137fc9d66459e3c413726
Aliaksandr96 <Aliaksandr.Hunko@gmail.com>
2022-10-24 15:51:19 +0200

    create app

---
+++ MVP_Test/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MVP_Test
+//
+//  Created by Aliaksandr Hunko on 24/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+}
+
